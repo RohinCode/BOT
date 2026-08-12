12831e8c09591cdf5a5e2d9ae8c56a08e4165a2d
RohinCode <Amir.reza.aab.2244@gmail.com>
2026-08-12 09:42:11 +0330

    Update projec4t

--- handlers/admin/showUsers.js
+++ handlers/admin/showUsers.js
@@ -1,4 +1,4 @@
-const User = require("../../models/users");
+const User = require("../../models/Users");
 const isAdmin = require("../../middlewares/isAdmin");
 const getUserStatus = require("../../utils/getUserStatus");
 
