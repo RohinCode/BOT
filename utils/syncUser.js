243af8feb50088a9a152f21b0c8292133c4a4b77
RohinCode <Amir.reza.aab.2244@gmail.com>
2026-08-12 20:14:35 +0330

    Update project

--- utils/syncUser.js
+++ utils/syncUser.js
@@ -1,41 +1,33 @@
-const User = require("../models/Users");
-const logger = require("./logger");
-
 module.exports = async function syncUser(ctx) {
   const User = require("../models/Users");
   const logger = require("./logger");
+  try {
+    let user = await User.findOne({
+      telegramId: ctx.from.id,
+    });
 
-  const syncUser = async (ctx) => {
-    try {
-      let user = await User.findOne({
+    if (!user) {
+      user = new User({
         telegramId: ctx.from.id,
+        name: ctx.from.first_name,
+        username: ctx.from.username,
       });
 
-      if (!user) {
-        user = new User({
-          telegramId: ctx.from.id,
-          name: ctx.from.first_name,
-          username: ctx.from.username,
-        });
-
-        await user.save();
-      } else {
-        await User.updateOne(
-          { telegramId: ctx.from.id },
-          { $set: { username: ctx.from.username, name: ctx.from.first_name } },
-        );
-      }
-    } catch (error) {
-      logger.error("SYNC USER ERROR", {
-        message: error.message,
-        stack: error.stack,
-        userId: ctx.from?.id,
-        username: ctx.from?.username,
-      });
-
-      throw error;
+      await user.save();
+    } else {
+      await User.updateOne(
+        { telegramId: ctx.from.id },
+        { $set: { username: ctx.from.username, name: ctx.from.first_name } },
+      );
     }
-  };
+  } catch (error) {
+    logger.error("SYNC USER ERROR", {
+      message: error.message,
+      stack: error.stack,
+      userId: ctx.from?.id,
+      username: ctx.from?.username,
+    });
 
-  module.exports = syncUser;
+    throw error;
+  }
 };
