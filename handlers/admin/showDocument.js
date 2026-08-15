5b33e4485be77a7c882f377fe0c11c0ac7a02847
RohinCode <Amir.reza.aab.2244@gmail.com>
2026-08-15 15:55:58 +0330

    Update project

--- handlers/admin/showDocument.js
+++ handlers/admin/showDocument.js
@@ -5,7 +5,10 @@ const getUserStatus = require("../../utils/getUserStatus");
 module.exports = (bot) => {
   bot.command("showDocument", isAdmin, async (ctx) => {
     const documents = await Payload.find().limit(22).sort({ createAt: -1 });
-
+    if (!documents) {
+      await ctx.reply("فایلی ذخیره نشده");
+      return false
+    }
     let message = "لیست فایل‌های ذخیره شده\n\n";
     for (const document of documents) {
       let payload = document.payload;
